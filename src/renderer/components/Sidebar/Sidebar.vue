27b23d46cebaaf0584c5f2f73c26cddf1619786d
j <atom10558@hotmail.com>
2018-03-15 23:52:20 +0800

    first commit

---
+++ src/renderer/components/Sidebar/Sidebar.vue
@@ -0,0 +1,13 @@
+<template>
+    <div id="app">
+    <sidebar
+      :categories="categories"
+      v-on:category-selected="setSelectedCategory">
+      <!-- bind 'selected-category event to the event handler setSelectedCategory' -->
+    </sidebar>
+    <bookmark-list
+      :bookmarks="bookmarks | filterByCategory selectedCategory"
+      :categories="categories">
+    </bookmark-list>
+  </div>
+</template>
